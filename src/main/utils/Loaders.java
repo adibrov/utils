4cf78cfe8405525cd92aa649861e86aeb36c234c
adibrov <alexander.dibroff@gmail.com>
2018-04-18 11:09:11 +0200

    loaders

--- src/main/utils/Loaders.java
+++ src/main/utils/Loaders.java
@@ -11,7 +11,7 @@ import java.nio.FloatBuffer;
 public class Loaders {
     public static ByteBuffer loadUnsignedShortSampleSpaceFromDisk(String pPath, int pDimX, int pDimY, int pDimZ) {
         System.out.println("Loading a SampleSpace from file " + pPath);
-        byte[] arr = new byte[2*pDimX * pDimY * pDimZ];
+        byte[] arr = new byte[2 * pDimX * pDimY * pDimZ];
 
         try (FileInputStream fis = new FileInputStream(pPath); BufferedInputStream bis = new BufferedInputStream(fis)) {
             long t1 = System.nanoTime();
@@ -19,18 +19,17 @@ public class Loaders {
 
             System.out.println(" loading: " + arr[12000] + " " + arr[13000] + " " + arr[20000]);
 
-//            }
+            //            }
             long t2 = System.nanoTime();
             System.out.println("---Buffering: yes. Loaded file " + pPath + " in: " + ((t2 - t1) / 1000000.) + " ms");
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }
-        finally {
+        } finally {
             System.out.println("Success.");
             System.out.println();
         }
-//        return arrInt;
+        //        return arrInt;
         ByteBuffer bb = ByteBuffer.wrap(arr);
         bb.rewind();
         return bb;
@@ -39,29 +38,28 @@ public class Loaders {
     public static ByteBuffer loadUnsignedByteSampleSpaceFromDisk(String pPath, int pDimX, int pDimY, int pDimZ) {
         System.out.println("Loading a SampleSpace from file " + pPath);
         byte[] arr = new byte[pDimX * pDimY * pDimZ];
-        short[] arrInt = new short[pDimX*pDimY*pDimZ];
+        short[] arrInt = new short[pDimX * pDimY * pDimZ];
 
         long h = 0;
         try (FileInputStream fis = new FileInputStream(pPath); BufferedInputStream bis = new BufferedInputStream(fis)) {
             long t1 = System.nanoTime();
             bis.read(arr);
 
-//            for (int i = 0; i < pDimX*pDimY*pDimZ; i++) {
-//                int b1 = arr[2*i];
-//                int b2 = arr[2*i+1];
-////                System.out.println("bytes b1 and b2: " + b1 + " " + b2);
-////                System.out.println("bytes b1 and b2: " + ((b1<<8)&mask1) + " " + (b2&mask2));
-////                System.out.println("bytes b1 and b2 sum: " + (((b1<<8)&mask1)  + (b2&mask2)));
-//
-////                System.out.println("just loaded: " + h);
-//            }
+            //            for (int i = 0; i < pDimX*pDimY*pDimZ; i++) {
+            //                int b1 = arr[2*i];
+            //                int b2 = arr[2*i+1];
+            ////                System.out.println("bytes b1 and b2: " + b1 + " " + b2);
+            ////                System.out.println("bytes b1 and b2: " + ((b1<<8)&mask1) + " " + (b2&mask2));
+            ////                System.out.println("bytes b1 and b2 sum: " + (((b1<<8)&mask1)  + (b2&mask2)));
+            //
+            ////                System.out.println("just loaded: " + h);
+            //            }
             long t2 = System.nanoTime();
             System.out.println("---Buffering: yes. Loaded file " + pPath + " in: " + ((t2 - t1) / 1000000.) + " ms");
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }
-        finally {
+        } finally {
             System.out.println("Success.");
             System.out.println();
         }
@@ -73,8 +71,8 @@ public class Loaders {
         File f = new File(pPath);
         long lFileSize = f.length();
 
-        byte[] arr = new byte[(int)lFileSize];
-        try ( FileInputStream fis = new FileInputStream(f); BufferedInputStream bis = new BufferedInputStream(fis)) {
+        byte[] arr = new byte[(int) lFileSize];
+        try (FileInputStream fis = new FileInputStream(f); BufferedInputStream bis = new BufferedInputStream(fis)) {
 
             long t1 = System.nanoTime();
             bis.read(arr);
@@ -83,12 +81,11 @@ public class Loaders {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }
-        finally {
+        } finally {
             System.out.println("Success.");
             System.out.println();
         }
-        return ByteBuffer.wrap(arr).order(ByteOrder.LITTLE_ENDIAN). asFloatBuffer().duplicate();
+        return ByteBuffer.wrap(arr).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer().duplicate();
     }
 
     public static void writeByteBufferToDisk(String pPath, ByteBuffer pByteBuffer) {
@@ -106,46 +103,42 @@ public class Loaders {
             System.out.println("---Buffering: yes. Write file " + pPath + " in: " + ((t2 - t1) / 1000000.) + " ms");
         } catch (Exception e) {
             e.printStackTrace();
-//            return null;
-        }
-        finally {
+            //            return null;
+        } finally {
             System.out.println("Success.");
             System.out.println();
         }
-//        return ByteBuffer.wrap(arr).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer();
+        //        return ByteBuffer.wrap(arr).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer();
     }
 
 
-    public static ByteBuffer loadNamedUnsignedShortSampleFromDisk(String pPathToFile){
+    public static ByteBuffer loadNamedUnsignedShortRawToByteBuffer(String pPathToFile) {
         System.out.println("[Utils]: --- loading unsigned short sample space from disk ---");
         System.out.println("[Utils]: parsing dimensions...");
         String lDirectoryPath = "";
-        String[] lSplitSlash = pPathToFile.split("/");
-        for (int i = 0; i < lSplitSlash.length; i++) {
-            lDirectoryPath += lSplitSlash[i];
-        }
 
-        File resImgDir = new File(lDirectoryPath);
+        int dims[] = parseDimensionsOfASampleSpace(pPathToFile);
 
+        System.out.println("[Utils]: Loading named unsigned short with dims: " + dims[0] + "x" + dims[1] + "x" + dims[2]);
 
-        String[] split1 = pPathToFile.split("_");
+        return loadUnsignedShortSampleSpaceFromDisk(pPathToFile, dims[0], dims[1], dims[2]);
+    }
+
+    public static ByteBufferedImage loadNamedUnsignedShortRawToByteBufferedImage(String pPathToFile) throws Exception{
+        int[] dims = parseDimensionsOfASampleSpace(pPathToFile);
+        return new ByteBufferedImage(loadNamedUnsignedShortRawToByteBuffer(pPathToFile),dims[0], dims[1], dims[2],2);
+    }
+
+    public static int[] parseDimensionsOfASampleSpace(String pPathToSampleSpace) {
+        System.out.println("[Utils]: parsing dimensions of the sample space - " + pPathToSampleSpace);
+        String[] split1 = pPathToSampleSpace.split("_");
         String suff1 = split1[split1.length - 1];
         System.out.println("[Utils]: file name is: " + suff1);
         String[] split2 = suff1.split("\\.");
-//        System.out.println("split2 size is: " + split2.length);
         String suff2 = split2[split2.length - 2];
-
-//        System.out.println(split1[split1.length - 1]);
-
-
-        String suff = suff2;
-//        System.out.println(split2.length);
         String[] dimsS = suff2.split("x");
 
         int[] dims = {Integer.parseInt(dimsS[0]), Integer.parseInt(dimsS[1]), Integer.parseInt(dimsS[2])};
-
-        System.out.println("[Utils]: Loading named unsigned short with dims: " + dims[0] + "x"+dims[1]+"x"+dims[2]);
-
-        return loadUnsignedShortSampleSpaceFromDisk(pPathToFile, dims[0], dims[1], dims[2]);
+        return dims;
     }
 }
